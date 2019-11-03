dd02fcdd99f2bc5e0d82f046ce5c8b015ebd388a
Wills Grave <will@keydigitalconsulting.com>
2019-11-03 13:25:58 -0500

    Add the friends.js file for the array of friends.

---
+++ app/data/friends.js
@@ -0,0 +1,34 @@
+var friends = [
+    {
+       name: "Michael Jordan",
+       photo: "https://statics.sportskeeda.com/editor/2018/03/a4a7b-1520474015-800.jpg",
+       scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
+    },
+    {
+      name: "Tom Hanks",
+      photo: "https://www.indiewire.com/wp-content/uploads/2017/02/shutterstock_7898420om.jpg?w=780",
+      scores: [4, 2, 5, 1, 3, 2, 2, 1, 3, 2]
+   },
+   {
+      name: "Charlize Theron",
+      photo: "https://hips.hearstapps.com/elleuk.cdnds.net/17/28/1600x800/landscape-1499952967-charliz-theron.jpg?resize=480:*",
+      scores: [5, 2, 2, 2, 4, 1, 3, 2, 5, 5]
+    },
+    {
+      name: "Seth Rogen",
+      photo: "https://pmcvariety.files.wordpress.com/2018/09/seth-rogen1.jpg?w=1000",
+      scores: [3, 3, 4, 2, 2, 1, 3, 2, 2, 3]
+    },
+    {
+      name: "Adam Sandler",
+      photo: "https://pmcdeadline2.files.wordpress.com/2019/07/shutterstock_editorial_10256298i.jpg?crop=0px%2C352px%2C3177px%2C1782px&resize=681%2C383",
+      scores: [4, 3, 4, 1, 5, 2, 5, 3, 1, 4]
+    },
+    {
+      name: "Halle Berry",
+      photo: "https://static.vibe.com/files/2019/05/halle-berry-praised-not-skipping-black-journalists-john-wick-3-red-carpet-1558104138-650x366.jpg",
+      scores: [4, 3, 4, 2, 4, 3, 5, 3, 5, 4]
+    }
+];
+
+module.exports = friends;
